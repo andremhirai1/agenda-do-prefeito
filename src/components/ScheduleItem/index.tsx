e50e0b270e0c94e949e8d5f7716333d2bb7e1d4d
André Massaki Hirai <56045734+andremhirai@users.noreply.github.com>
2023-12-29 15:22:13 -0300

    calendar

--- src/components/ScheduleItem/index.tsx
+++ src/components/ScheduleItem/index.tsx
@@ -9,7 +9,7 @@ const ScheduleItem = ({ item }: { item: Item }) => {
     return (
         <div className='psp-schedule-item'>
             <span className='psp-schedule-item-hours'>{formatHours(item.startDate)}</span>
-            <p className='psp-schedule-item-title'>{limitText(item.title, 200)}</p>
+            <a className='psp-schedule-item-title' href={`/l/${item.id}`}>{limitText(item.title, 200)}</a>
             <span className='psp-schedule-item-location'>{item.location}</span>
         </div>
     );
