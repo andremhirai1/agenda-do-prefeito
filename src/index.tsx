accc4d26c907c8979661b9b60b24dd03bab0cc61
André Massaki Hirai <56045734+andremhirai@users.noreply.github.com>
2023-12-29 15:14:01 -0300

    calendar

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+class WebComponent extends HTMLElement {
+  // eslint-disable-next-line @typescript-eslint/no-useless-constructor
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    ReactDOM.createRoot(this).render(
+      <App />
+    );
+  }
+}
+
+const ELEMENT_ID = 'custom-calendar';
+
+if (!customElements.get(ELEMENT_ID)) {
+  customElements.define(ELEMENT_ID, WebComponent);
+}
 
